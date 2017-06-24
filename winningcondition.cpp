cf20c712461f37403ad7e5efba8f7ef392b4771a
stabletech <dale03ion@gmail.com>
2017-06-24 23:52:19 +0200

    winning condition check

---
+++ winningcondition.cpp
@@ -0,0 +1,7 @@
+#include "winningcondition.h"
+
+winningCondition::winningCondition()
+{
+
+}
+
---
+++ winningcondition.h
@@ -0,0 +1,11 @@
+#ifndef WINNINGCONDITION_H
+#define WINNINGCONDITION_H
+
+
+class winningCondition
+{
+public:
+    winningCondition();
+};
+
+#endif // WINNINGCONDITION_H
